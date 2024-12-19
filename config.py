b9172708a2a81f6a0d049c86146b77176cd67fff
noeru_desu <3203911711@qq.com>
2024-12-20 00:00:59 +0800

    忘记调用父级初始化了 :(

--- config.py
+++ config.py
@@ -261,6 +261,7 @@ class InstanceConfig(LLMConfig):
         return shared.plugin_config.system_prompts.get(self.bot_name)
 
     def __init__(self, chat_key: str) -> None:
+        super().__init__()
         self.chat_key = chat_key
 
     def apply_yaml(self) -> None:
